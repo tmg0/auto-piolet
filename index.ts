4ffb966517156834b9abfdd773d2c74afaa5645d
zekunjin <zekun.jin@foxmail.com>
2023-02-24 17:32:05 +0800

    feat: add interval 50ms

--- index.ts
+++ index.ts
@@ -1,4 +1,40 @@
 #! /usr/bin/env node
 
-import { } from 'logitech-g29'
-import {} from 'node-global-key-listener'
+import g from 'logitech-g29'
+import { GlobalKeyboardListener } from 'node-global-key-listener'
+import consola from 'consola'
+
+const v = new GlobalKeyboardListener()
+
+enum Keyboard {
+  SPACE = 'SPACE'
+}
+
+let isRunning = false
+let isLock = false
+
+const toggle = () => {
+  if (isLock) { return }
+
+  isLock = true
+  isRunning = !isRunning
+  setTimeout(() => { isLock = false }, 1000)
+}
+
+v.addListener(({ name }) => {
+  if (name === Keyboard.SPACE) { toggle() }
+})
+
+// g.connect()
+
+setInterval(() => {
+  if (!isRunning) {
+    // g.autoCenterOn()
+    // g.forceOff()
+    return
+  }
+
+  consola.info('Running...')
+  // g.forceConstant(-0.5, -180)
+  // g.forceConstant(-0.5, 180)
+}, 50)
